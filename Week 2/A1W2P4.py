c346ec60099ee4f04f63a2c2ed2b00fb890afcc2
Tommy Chen <phenovaa@icloud.com>
2025-03-10 15:27:08 +0100

    Update A1W2P4.py

--- Week 2/A1W2P4.py
+++ Week 2/A1W2P4.py
@@ -8,7 +8,7 @@ def triangle_type(x):
     int_x3 = int(x3)
 
     if int_x1 == int_x2 and int_x2 == int_x3 and int_x3 == int_x1:
-        print("equilateral")
+        print("Equilateral")
     elif int_x1 == int_x2 or int_x2 == int_x3 or int_x3 == int_x1:
         print("Isosceles")
     else:
