97621e6c6af2f1a3b35bea295a8e0ba67e7a4d0e
Tommy Chen <phenovaa@icloud.com>
2025-03-22 12:46:37 +0100

    Update A1W3P4.py

--- Week 3/A1W3P4.py
+++ Week 3/A1W3P4.py
@@ -7,7 +7,7 @@ def celsius_to_fahrenheit():
     for i in range(10):
         for b in range(1):
             print(celsius, fahrenheit, end=" ")
-            celsius+= 10
+            celsius += 10
             fahrenheit = (celsius * 9/5) + 32
             fahrenheit = int(fahrenheit)
         print()
